e51fefe4b04a5fb8635596c92f9e305e960dd5c8
Gui11epio <guilherme.laiber@gmail.com>
2025-09-15 15:40:14 -0300

    HomeController e os fragments adicionados

---
+++ src/main/java/web/HomeController.java
@@ -0,0 +1,18 @@
+package web;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+@AllArgsConstructor
+public class HomeController {
+
+    @GetMapping
+    public String home(Model model) {
+        return "home";
+    }
+}
