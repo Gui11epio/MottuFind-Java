788ce23d05c201e8cf55f27f47f6271bc8fb480f
Gui11epio <guilherme.laiber@gmail.com>
2025-09-25 16:56:23 -0300

    reiniciando o security

--- src/main/java/br/com/fiap/MottuFind/model/Role.java
+++ src/main/java/br/com/fiap/MottuFind/model/Role.java
@@ -3,17 +3,8 @@ package br.com.fiap.MottuFind.model;
 import jakarta.persistence.*;
 import lombok.*;
 
-@Entity
-@Table(name = "tb_roles")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Role {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(nullable = false, unique = true)
-    private String nome; // ex: "ROLE_ADMIN", "ROLE_USER"
+public enum Role {
+    ROLE_ADMIN,
+    ROLE_USER,
 }
--- src/main/java/br/com/fiap/MottuFind/model/Usuario.java
+++ src/main/java/br/com/fiap/MottuFind/model/Usuario.java
@@ -5,7 +5,6 @@ import lombok.*;
 import java.util.Set;
 
 @Entity
-@Table(name = "tb_usuarios")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
@@ -15,17 +14,9 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
     private String username;
 
-    @Column(nullable = false)
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
-    @JoinTable(
-            name = "tb_usuarios_roles",
-            joinColumns = @JoinColumn(name = "usuario_id"),
-            inverseJoinColumns = @JoinColumn(name = "role_id")
-    )
-    private Set<Role> roles;
+    private Role roles;
 }
