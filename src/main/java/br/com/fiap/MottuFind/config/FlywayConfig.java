3a820e97054dccecca1f57d7c4030e5719686362
Gui11epio <guilherme.laiber@gmail.com>
2025-09-28 23:14:33 -0300

    Flyway funcionando

---
+++ src/main/java/br/com/fiap/MottuFind/config/FlywayConfig.java
@@ -0,0 +1,27 @@
+package br.com.fiap.MottuFind.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateProperties;
+import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.annotation.Order;
+
+@Configuration
+@EnableConfigurationProperties({HibernateProperties.class, JpaProperties.class})
+public class FlywayConfig {
+
+    /**
+     * Esta configuração garante que o Flyway execute antes do JPA
+     */
+    @Bean
+    @Primary
+    public FlywayMigrationStrategy flywayMigrationStrategy() {
+        return flyway -> {
+            // Executa as migrações do Flyway primeiro
+            flyway.migrate();
+        };
+    }
+}
